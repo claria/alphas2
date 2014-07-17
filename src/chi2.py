5d4599adda592fbd084b86fad513d38cec813a76
Georg Sieber <g.sieber@gmail.com>
2014-07-17 22:52:47 +0200

    changed all np.matrix to np.ndarray objects

--- src/chi2.py
+++ src/chi2.py
@@ -6,11 +6,11 @@ from abc import ABCMeta, abstractmethod
 class Chi2(object):
     __metaclass__ = ABCMeta
 
-    def __init__(self, measurement=None):
+    def __init__(self, dataset=None):
         self._chi2 = 0.0
-        self._measurement = measurement
-        self._data = self._measurement.data
-        self._theory = self._measurement.theory
+        self._dataset = dataset
+        self._data = self._dataset.data
+        self._theory = self._dataset.theory
 
     def get_chi2(self):
         self._calculate_chi2()
@@ -23,22 +23,28 @@ class Chi2(object):
 
 class Chi2Cov(Chi2):
 
-    def __init__(self, measurement=None):
-        super(Chi2Cov, self).__init__(measurement)
+    def __init__(self, dataset=None):
+        super(Chi2Cov, self).__init__(dataset)
 
     def _calculate_chi2(self):
-        inv_matrix = np.matrix(self._measurement.get_cov_matrix()).getI()
-        residual = np.matrix(self._data - self._theory)
-        self._chi2 = (residual * inv_matrix * residual.getT())[0, 0]
+        inv_matrix = np.linalg.inv(self._dataset.get_cov_matrix())
+        residual = self._data - self._theory
+        self._chi2 = np.inner(np.inner(residual, inv_matrix), residual)
 
 
 class Chi2Nuisance(Chi2):
+    """Calculate Chi2 with different kinds of uncertainties.
 
-    def __init__(self, measurement):
-        super(Chi2Nuisance, self).__init__(measurement)
-        self._cov_matrix = self._measurement.get_cov_matrix(unc_treatment=('cov',))
-        self._inv_matrix = np.matrix(self._cov_matrix).getI()
-        self._beta = self._measurement.get_uncert_list(unc_treatment=('nuis',))
+    Uncorrelated uncertainties and partly correlated uncertanties are
+    treated using a covariance matrix  while fully correlated
+    uncertainties are treated using nuisance parameters.
+    """
+    def __init__(self, dataset):
+        super(Chi2Nuisance, self).__init__(dataset)
+        self._cov_matrix = self._dataset.get_cov_matrix(unc_treatment='cov')
+        self._beta = self._dataset.get_uncert_list(unc_treatment='nuis')
+
+        self._inv_matrix = np.linalg.inv(self._cov_matrix)
 
         self._chi2_correlated = 0.0
         self._theory_mod = None
@@ -66,7 +72,7 @@ class Chi2Nuisance(Chi2):
         return self._theory_mod
 
     def get_ndof(self):
-        return self._measurement.data.shape[0]
+        return self._dataset.data.shape[0]
 
     def save_nuisance_parameters(self, filename):
         nuisance_parameters = self.get_nuisance_parameters()
@@ -82,38 +88,19 @@ class Chi2Nuisance(Chi2):
                     key, nuisance_parameters[key]))
 
     def _calculate_chi2(self):
-        """Calculate Chi2 with different kinds of uncertainties.
-
-        Uncorrelated uncertainties and partly correlated uncertanties are
-        treated using a covariance matrix  while fully correlated
-        uncertainties are treated using nuisance parameters.
-        """
 
         chi2_corr = 0.0
         nbeta = len(self._beta)
-        # npoints = len(data)
         b = np.zeros((nbeta,))
-        a = np.matrix(np.identity(nbeta))
-        # inv_matrix = cov_matrix.getI()
+        a = np.identity(nbeta)
 
         # Calculate Bk and Akk' according to paper: PhysRevD.65.014012
-        # Implementation similar to h1fitter
+        # Implementation adapted to the one of the HERAFitter
+        # TODO: Get rid of one loop by switching to ndarray of _beta
         for k in range(0, nbeta):
-            b[k] = (np.matrix(self._data - self._theory)
-                    * self._inv_matrix * np.matrix(self._beta[k].get_arr()).getT())
-            # Better readable but much slower
-            # for l in range(0,npoints):
-            #    for i in range(0,npoints):
-            #        B[k] += data[l] * syst_error[k][l]*
-            #                (data[i]-theory[i]) * inv_matrix[l,i]
+            b[k] = (self._data - self._theory) * self._inv_matrix * self._beta[k].get_arr().T()
             for j in range(0, nbeta):
-                a[k, j] += (np.matrix(self._beta[j].get_arr()) * self._inv_matrix *
-                            np.matrix(self._beta[k].get_arr()).getT())
-                # Better readable but way slower
-                # for l in range(0,npoints):
-                #    for i in range(0,npoints):
-                #        A[k,j] += syst_error[k][i] * data[i] *
-                #                 syst_error[j][l] * data[l] * inv_matrix[l,i]
+                a[k, j] += self._beta[j].get_arr() * self._inv_matrix * self._beta[k].get_arr().T()
 
         # Multiply by -1 so nuisance parameters correspond to shift
         # noinspection PyTypeChecker
@@ -123,10 +110,11 @@ class Chi2Nuisance(Chi2):
         for k in range(0, nbeta):
             self._theory_mod = self._theory_mod - self._r[k] * self._beta[k].get_arr()
             chi2_corr += self._r[k] ** 2
+        # Add also external nuisance parameter, which are fitted by Minuit
         for k in range(0, len(self._beta_external)):
             self._theory_mod = self._theory_mod - self._r_external[k] * self._beta_external[k].get_arr()
             chi2_corr += self._r_external[k] ** 2
 
-        residual_mod = np.matrix(self._data - self._theory_mod)
-        self._chi2 = (residual_mod * self._inv_matrix * residual_mod.getT())[0, 0]
+        residual_mod = self._data - self._theory_mod
+        self._chi2 = np.inner(np.inner(residual_mod, self._inv_matrix), residual_mod)
         self._chi2 += chi2_corr
